e8578840fd0a5b37dd0fd5b76cdf8b6cda5db3dc
Eduardo Barbosa <eduardocbarbosa1998@gmail.com>
2026-05-07 19:51:47 -0500

    feat(karimo): Stripe Foundation [T01]

---
+++ lib/stripe.ts
@@ -0,0 +1,10 @@
+import Stripe from 'stripe'
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+
+export const stripe = stripeSecretKey
+  ? new Stripe(stripeSecretKey, {
+      apiVersion: '2026-04-22.dahlia',
+      typescript: true,
+    })
+  : null
